5d6d7147d25a7e91f40867238c0678d15d576852
Rohan Andharia <93028154+rohan2781@users.noreply.github.com>
2025-10-29 03:24:10 +0530

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS
    Restrict allowed hosts and add CSRF trusted origins.

--- godseye/settings.py
+++ godseye/settings.py
@@ -26,9 +26,15 @@ SECRET_KEY = 'django-insecure-0asp7^cn87h!fwlde$&385=i!av=i$7&a6kdtr28l+ldi6(+fr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'godseye-aeyq.onrender.com',
+]
 
 
+# CSRF trusted origins (important for POST forms)
+CSRF_TRUSTED_ORIGINS = [
+    'https://godseye-aeyq.onrender.com',
+]
 # Application definition
 
 INSTALLED_APPS = [
