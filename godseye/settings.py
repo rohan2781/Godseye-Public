0389a139952875c0775f95aa4bdd8d957fc4dc63
Rohan Andharia <93028154+rohan2781@users.noreply.github.com>
2025-10-29 03:10:10 +0530

    Set DEBUG to False and allow all hosts
    Changed DEBUG setting to False and updated ALLOWED_HOSTS.

--- godseye/settings.py
+++ godseye/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0asp7^cn87h!fwlde$&385=i!av=i$7&a6kdtr28l+ldi6(+fr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
