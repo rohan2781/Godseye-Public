e1b9c561e2d7056052b43916312d7605e96a1f61
rohan2781 <rohanandharia@gmail.com>
2025-11-19 11:15:39 +0530

    Trades handling for a day longer

--- godseye/views.py
+++ godseye/views.py
@@ -954,30 +954,29 @@ def positions(req):
                         #     ltp=ltp_cache[ltp_key]
                         # except:
                         #     ltp=0
-
-                        filtered_trades = trades[trades['trading_symbol'] == row["trading_symbol"]]
-                        
-                        # Sum the trade_price column of the filtered trades
-                        # total_value = (filtered_trades['trade_quantity'] * filtered_trades['trade_price']).sum()
-                        average_trade_price = filtered_trades['trade_price'].mean()
-
-                        if row['net_quantity']>0:
-                            quantity = abs(int(row['net_quantity']))
-                            price = average_trade_price
-                            side = 'BUY'
-                        else:
-                            quantity = abs(int(row['net_quantity']))
-                            price = average_trade_price
-                            side = 'SELL'
-
-                        # if float(row['cf_sell_quantity']) != 0:
-                        #     quantity = row['cf_sell_quantity']
-                        #     price = float(row['actual_average_sell_price'])
-                        #     side = 'SELL'
-                        # else:
-                        #     quantity = row['cf_buy_quantity']
-                        #     price = float(row['actual_average_buy_price'])
-                        #     side = 'BUY'
+                        try:
+                            filtered_trades = trades[trades['trading_symbol'] == row["trading_symbol"]]
+                            
+                            # Sum the trade_price column of the filtered trades
+                            # total_value = (filtered_trades['trade_quantity'] * filtered_trades['trade_price']).sum()
+                            average_trade_price = filtered_trades['trade_price'].mean()
+                            if row['net_quantity']>0:
+                                quantity = abs(int(row['net_quantity']))
+                                price = average_trade_price
+                                side = 'BUY'
+                            else:
+                                quantity = abs(int(row['net_quantity']))
+                                price = average_trade_price
+                                side = 'SELL'
+                        except:
+                            if float(row['cf_sell_quantity']) != 0:
+                                quantity = row['cf_sell_quantity']
+                                price = float(row['actual_average_sell_price'])
+                                side = 'SELL'
+                            else:
+                                quantity = row['cf_buy_quantity']
+                                price = float(row['actual_average_buy_price'])
+                                side = 'BUY'
                         pnl=round((float(ltp) - float(price)) * float(quantity)  if side == 'BUY' else (float(price) - (float(ltp))) * float(quantity),2)
                         pos.loc[len(pos)] = [instrument, expiry, strike, type_, qty, ltp, token, exchange, pnl, quantity, price, side]
 
