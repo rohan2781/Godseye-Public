6c32a321613e296dd87e707a1ddd5b6bb69db51c
rohan2781 <rohanandharia@gmail.com>
2026-02-18 00:06:01 +0530

    login error resolved

--- godseye/views.py
+++ godseye/views.py
@@ -1947,22 +1947,26 @@ def home(req):
         return redirect('/home')
 
 def login(req):
-    
-    if req.method=='POST':
-        username=req.POST['username'].lower()
-        password=req.POST['password']
-        user=Users.objects.filter(username=username,password=password)
-        if user.exists():
-            user=user.first()
-            token=f"user_{user.userid}"
-            r.set(name=str(user.userid),value='1',ex=25200)
-            # return HttpResponse("Logged In")
-            response= redirect('/home')
-            response.set_cookie('x-auth-token', token, httponly=True, secure=True, samesite='Strict')
-            return response
+    try:
+        if req.method=='POST':
+            username=req.POST['username'].lower()
+            password=req.POST['password']
+            user=Users.objects.filter(username=username,password=password)
+            if user.exists():
+                user=user.first()
+                token=f"user_{user.userid}"
+                r.set(name=str(user.userid),value='1',ex=25200)
+                # return HttpResponse("Logged In")
+                response= redirect('/home')
+                response.set_cookie('x-auth-token', token, httponly=True, secure=True, samesite='Strict')
+                return response
 
-        else:
-            messages.info(req,'Invalid Credentials')
+            else:
+                messages.info(req,'Invalid Credentials')
+    except Exception as e:
+        print(e)
+
+    
     
     return render(req,'login.html')
 
